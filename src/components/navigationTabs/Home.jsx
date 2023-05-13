21e1aaaad828a87b3f33fc9aaafaf72817e9eee6
ekim.coskun <ekim.coskun@path.com.tr>
2023-05-13 11:48:08 +0300

    Added typewriter and Home page completed

--- src/components/navigationTabs/Home.jsx
+++ src/components/navigationTabs/Home.jsx
@@ -1,7 +1,21 @@
+import Typewriter from "typewriter-effect";
+import background from "../../assets/background.jpg";
 function Home() {
   return (
-    <div className="page-container">
-      <h1>Home</h1>
+    <div className="page-container" style={{ backgroundImage: `url(${background})` }}>
+      <div className="home-texts">
+        <div className="typewriter">
+          <p>Welcome !</p>
+          <Typewriter
+            options={{
+              strings: ["I am Ekim Coskun.", "I am a Full Stack Developer."],
+              autoStart: true,
+              loop: true,
+              delay: 50,
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
