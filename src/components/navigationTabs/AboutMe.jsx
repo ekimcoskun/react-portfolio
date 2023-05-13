0d1e9f137bf9167f10af3b45611fa8429b3c7bce
ekim.coskun <ekim.coskun@path.com.tr>
2023-05-13 14:51:28 +0300

    Skills Page Added

--- src/components/navigationTabs/AboutMe.jsx
+++ src/components/navigationTabs/AboutMe.jsx
@@ -1,7 +1,44 @@
 function AboutMe() {
+  const handlePDFButton = () => {
+    fetch("CV_EkimCoskun.pdf").then((response) => {
+      response.blob().then((blob) => {
+        const fileURL = window.URL.createObjectURL(blob);
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "CV_EkimCoskun.pdf";
+        alink.click();
+      });
+    });
+  };
   return (
-    <div className="page-container" style={{ background: "red" }}>
-      <h1>AboutMe</h1>
+    <div className="page-container">
+      <div className="aboutme-container">
+        <p className="text-title">About Me</p>
+        <div style={{ display: "flex" }}>
+          <div className="text-container">
+            <p className="text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit modi ipsa numquam
+              perspiciatis rem ab odio rerum exercitationem? Vitae odio labore, molestias dolor
+              optio qui sint ratione voluptatum dignissimos maiores.
+            </p>
+          </div>
+
+          <div className="aboutme-details">
+            <p>
+              <b>Name:</b> Ekim Coskun
+            </p>
+            <p>
+              <b>Email:</b> ekim.coskun19@gmail.com
+            </p>
+            <p>
+              <b>Age:</b> 24
+            </p>
+            <button className="button-85" onClick={handlePDFButton}>
+              Download CV
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/navigationTabs/Skills.jsx
+++ src/components/navigationTabs/Skills.jsx
@@ -1,7 +1,41 @@
+import javascript from "../../assets/skills/javascript.png";
+import react from "../../assets/skills/react.png";
+import html from "../../assets/skills/html.png";
+import css from "../../assets/skills/css.png";
+import nodejs from "../../assets/skills/nodejs.png";
+import expressjs from "../../assets/skills/expressjs.png";
+import mongodb from "../../assets/skills/mongodb.png";
+import mysql from "../../assets/skills/mysql.png";
+import git from "../../assets/skills/git.png";
+import cplus from "../../assets/skills/c++.png";
+import typescript from "../../assets/skills/typescript.png";
+import kafka from "../../assets/skills/kafka.png";
+import shopify from "../../assets/skills/shopify.png";
+import javaspring from "../../assets/skills/javaspring.png";
+import java from "../../assets/skills/java.png";
 function Skills() {
   return (
     <div className="page-container">
-      <h1>Skills</h1>
+      <div className="skills-container">
+        <p className="text-title">Skills</p>
+        <div className="skill-logos">
+          <img className="skill-logo" src={javascript} alt="javascript" />
+          <img className="skill-logo" src={react} alt="react" />
+          <img className="skill-logo" src={html} alt="html" />
+          <img className="skill-logo" src={css} alt="css" />
+          <img className="skill-logo" src={nodejs} alt="nodejs" />
+          <img className="skill-logo" src={expressjs} alt="expressjs" />
+          <img className="skill-logo" src={mongodb} alt="mongodb" />
+          <img className="skill-logo" src={mysql} alt="mysql" />
+          <img className="skill-logo" src={git} alt="git" />
+          <img className="skill-logo" src={cplus} alt="c++" />
+          <img className="skill-logo" src={typescript} alt="typescript" />
+          <img className="skill-logo" src={kafka} alt="kafka" />
+          <img className="skill-logo" src={shopify} alt="shopify" />
+          <img className="skill-logo" src={javaspring} alt="javaspring" />
+          <img className="skill-logo" src={java} alt="java" />
+        </div>
+      </div>
     </div>
   );
 }
