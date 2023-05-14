859163e96b9d54d807bef0f4d3754ed0e571833f
ekim.coskun <ekim.coskun@path.com.tr>
2023-05-14 16:18:47 +0300

    Added Contact and Resume Page

--- src/components/navigationTabs/Contact.jsx
+++ src/components/navigationTabs/Contact.jsx
@@ -1,7 +1,70 @@
+import {
+  AiFillPhone,
+  AiOutlineMail,
+  AiFillGithub,
+  AiOutlineInstagram,
+  AiFillFacebook,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+  AiFillCaretRight,
+} from "react-icons/ai";
+
 function Contact() {
   return (
     <div className="page-container">
-      <h1>Contact</h1>
+      <div className="contact-container">
+        <p className="text-title">Contact</p>
+        <div className="contact-info">
+          <div className="single-info">
+            <AiFillCaretRight />
+            <p className="single-text">Izmir/Tire - TURKEY</p>
+          </div>
+          <div className="single-info">
+            <AiFillCaretRight />
+            <p className="single-text">Istanbul/Kartal - TURKEY</p>
+          </div>
+          <div className="single-info">
+            <AiFillPhone color="white" size={"30px"} />
+            <p className="single-text"> +90 551 220 7005</p>
+          </div>
+          <div className="single-info">
+            <AiOutlineMail color="white" size={"30px"} />
+            <p className="single-text"> ekim.coskun19@gmail.com</p>
+          </div>
+        </div>
+        <p className="contact-title">Follow Me</p>
+        <div className="navigation-footer">
+          <AiFillGithub
+            color="white"
+            size={"30px"}
+            onClick={() => window.open("https://github.com/ekimcoskun", "_blank")}
+            className="icon"
+          />
+          <AiFillFacebook
+            color="white"
+            size={"30px"}
+            onClick={() => window.open("https://www.facebook.com/trailer07", "_blank")}
+            className="icon"
+          />
+          <AiOutlineInstagram
+            color="white"
+            size={"30px"}
+            onClick={() => window.open("https://www.instagram.com/ekimcoskun", "_blank")}
+          />
+          <AiFillLinkedin
+            color="white"
+            size={"30px"}
+            onClick={() => window.open("https://www.linkedin.com/in/ekimcoskun/", "_blank")}
+            className="icon"
+          />
+          <AiFillTwitterCircle
+            color="white"
+            size={"30px"}
+            onClick={() => window.open("https://twitter.com/ekim_coskun", "_blank")}
+            className="icon"
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/navigationTabs/Resume.jsx
+++ src/components/navigationTabs/Resume.jsx
@@ -1,7 +1,45 @@
 function Resume() {
   return (
     <div className="page-container">
-      <h1>Resume</h1>
+      <div className="resume-container">
+        <p className="text-title">Resume</p>
+        <div className="resume">
+          <div className="resume-education">
+            <p className="second-title">Education</p>
+            <div className="single-resume">
+              <p className="long">2016-2022</p>
+              <div>
+                <p className="field">Electrical and Electronics Engineering</p>
+                <p className="company">Eskisehir Osmangazi University</p>
+              </div>
+            </div>
+          </div>
+          <div className="resume-experience">
+            <p className="second-title">Experience</p>
+            <div className="single-resume">
+              <p className="long">JUNE - 2022 / AUG - 2022</p>
+              <div>
+                <p className="field">Software Developer Intern</p>
+                <p className="company">NETAS</p>
+              </div>
+            </div>
+            <div className="single-resume">
+              <p className="long">OCT - 2022 / DEC - 2022</p>
+              <div>
+                <p className="field">Software Developer Intern</p>
+                <p className="company">PATH Product & Software House</p>
+              </div>
+            </div>
+            <div className="single-resume">
+              <p className="long">DEC - 2022 / PRESENT</p>
+              <div>
+                <p className="field">Full Stack Developer</p>
+                <p className="company">PATH Product & Software House</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
